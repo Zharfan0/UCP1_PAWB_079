18ed4d7085e5fdfc5837399fd05862a9afc106eb
ZHARFAN <zharfanwidyadana@gmail.com>
2024-11-23 18:28:39 +0700

    tambah main.ejs

--- app/model/ArrPupuk.js
+++ app/model/ArrPupuk.js
@@ -3,6 +3,6 @@ let ArrPupuk = [];
 module.exports = {
     getAll: () => ArrPupuk,
     add: (pupuk) => ArrPupuk.push(pupuk),
-    update: (index, pupuk) => (animals[index] = pupuk),
+    update: (index, pupuk) => (pupuk[index] = pupuk),
     delete: (index) => ArrPupuk.splice(index, 1),
 };
